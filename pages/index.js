cc0e1ea0132ec8307a4bf484fae7c31b592a4be4
vipinjoshi <vipiswaiting@live.com>
2020-02-03 17:26:44 +0530

    add new component post link apply css local and global both

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
 import Layout from '../components/Layout';
+import PostLink from '../components/PostLink';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
 const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(show => (
-        <li key={show.id}>
-          <Link href="/p/[id]" as={`/p/${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(show => (
+                <PostLink key={show.id} href="/p/[id]" key={show.id} show={show} />
+            ))}
+        </ul>
+        <style jsx>{`
+        h1
+         {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+      `}</style>
+    </Layout>
 );
 
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
 
-  console.log(`Show data fetched. Count: ${data.length}`);
-  return {
-    shows: data.map(entry => entry.show)
-  };
+    return {
+        shows: data.map(entry => entry.show)
+    };
 };
 
 export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,20 +1,42 @@
 import Layout from '../../components/Layout';
+import Markdown from 'react-markdown';
 import fetch from 'isomorphic-unfetch';
 
 const Post = props => (
   <Layout>
     <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    <div className="markdown">
+      <Markdown source={props.show.summary.replace(/<[/]?[pb]>/g, '')} />
+    </div>
+    <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+
     {props.show.image ? <img src={props.show.image.medium} /> : null}
   </Layout>
 );
 
-Post.getInitialProps = async (context)=> {
+Post.getInitialProps = async (context) => {
   const { id } = context.query;
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
   const show = await res.json();
-
-  console.log(`Fetched show: ${show.name}`);
 
   return { show };
 };
