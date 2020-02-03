cbad1d597f6fa33661dd0bdfb2d3b309340a9898
vipinjoshi <vipiswaiting@live.com>
2020-02-03 12:14:51 +0530

    use useRouter to fetch querystring parameter to get content in dynnamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import Link from 'next/link';
 import withLayout from '../components/MyLayout';
 
-const Page = () => <p>Hello Next.js</p>;
+const Page = () => <>
+    <h1>My Blog</h1>
+    <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+    </ul>
+</>
+
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
 
 export default withLayout(Page);
